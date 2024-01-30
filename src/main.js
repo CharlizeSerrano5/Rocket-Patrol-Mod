6bff880329cebd0fa707d118e955286fda5072b9
Charlize <extrabutterychar@gmail.com>
2024-01-29 18:02:19 -0800

    Adding Initial Files

---
+++ src/main.js
@@ -0,0 +1,23 @@
+//Charlize Serrano
+//mod title (e.g. Rocket Patrol Reloaded IV: The Rocketing)
+//12 hours
+//the mods you chose from the list above, their point values, and, if necessary, an explanation of their implementation
+
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+let game = new Phaser.Game(config)
+
+// reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
+
+let easyGameTimer = 60000
+let hardGameTimer = 45000
+//let subtracted = 5000
